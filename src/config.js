adfe04457b18a6f492e4fbc62dfdf2007c7c2655
Nicolas Potier <nicolaspotier97@gmail.com>
2020-08-11 21:29:14 -0500

    environments Variables

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const FirebaseConfig = {
-    apiKey: "AIzaSyCOp2cksaagiH0dPZgJqWzZ8IKvQi6QM-w",
-    authDomain: "telemetryiot.firebaseapp.com",
-    databaseURL: "https://telemetryiot.firebaseio.com",
-    projectId: "telemetryiot",
-    storageBucket: "telemetryiot.appspot.com",
-    messagingSenderId: "422949786515",
-    appId: "1:422949786515:web:d55f2d246e11a9ed12ce66",
-    measurementId: "G-HYTC5WF44S"
-}; 
+    apiKey: process.env.REACT_APP_APIKEY_FIREBASE,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN_FIREBASE,
+    databaseURL: process.env.REACT_APP_DATABASE_FIREBASE,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STOREBUCKET_FIREBASE,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID_FIREBASE,
+    appId: process.env.REACT_APP_APPID_FIREBASE,
+    measurementId: process.env.REACT_APP_MEASUREMENTID_FIREBASE,
+};
 
 export default FirebaseConfig;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter  as Router,
-  // eslint-disable-next-line 
-  HashRouter,
+  BrowserRouter as Router, 
   Switch,
   Route,
   // eslint-disable-next-line
-  Redirect 
-} from 'react-router-dom';
-// eslint-disable-next-line
-import NoFound from "../pages/noFound";
-// eslint-disable-next-line
+  Redirect
+} from 'react-router-dom'; 
+import NoFound from "../pages/noFound"; 
 import Maintenance from '../pages/maintenance';
-import Login  from '../pages/login';
-import Dashboard  from '../pages/dashboard';
-//import { useFirebaseApp } from 'reactfire'
+import Login from '../pages/login';
+import Dashboard from '../pages/dashboard';
+import { useFirebaseApp } from 'reactfire';
 
-class Routes extends Component {
-  //const firebase = useFirebaseApp();
-  //console.log(firebase);
-  render() {
+// class Routes extends Component {
+  
+  // render() {
+    function Routes(){
+    const firebase = useFirebaseApp();
+    console.log(firebase); 
+
+    
     return (
-      <> 
-        <Router   //HashRouter  basename={process.env.PUBLIC_URL + '/'}
-        >
+      <>
+        <Router   >
           <Switch>
-          {/* <Redirect exact from="/TelemetryIot"  to="/TelemetryIot/login" /> */}
-             
+            {/* <Redirect exact from="/TelemetryIot"  to="/TelemetryIot/login" /> */}
+
             {/* <Route exact path='/TelemetryIot/dashboard' component={Dashboard} /> */}
-            <Route path="/TelemetryIot/dashboard" render={(props) => <Dashboard {...props} />} /> 
+            <Route path="/TelemetryIot/dashboard" render={(props) => <Dashboard {...props} />} />
             <Route exact path='/TelemetryIot/login' component={Login} />
-            <Route exact path='/' component={Maintenance} /> 
+            <Route exact path='/' component={Maintenance} />
             <Route component={NoFound} />
           </Switch>
         </Router>
       </>
     )
   }
-} export default Routes; 
+ export default Routes; 
